66c9ad49d9026a2a71bf4248025691a27500fabb
antonio <antonio@pykiss.com>
2019-08-01 13:05:29 +0200

    publish

---
+++ index.js
@@ -0,0 +1,26 @@
+export default async function fetchHelper (url, options) {
+  url = await url
+  if (Array.isArray(url)) {
+    options = url[1]
+    url = url[0]
+  }
+  var response = await fetch(url, options).catch(e => ({
+    error: e
+  }))
+
+  if (response.error) {
+    return [null, response.error]
+  }
+
+  if (!response.ok) {
+    return [null, response]
+  }
+
+  try {
+    response = await response.json()
+  } catch (e) {
+    return [null, e]
+  }
+
+  return [response, undefined]
+}
